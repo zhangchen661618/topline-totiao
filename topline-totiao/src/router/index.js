ac5b02368fa94f15e25dd5faca94fd505fdb8209
张晨 <502030647@example.com>
2019-09-04 22:13:18 +0800

    首页-布局

--- topline-totiao/src/router/index.js
+++ topline-totiao/src/router/index.js
@@ -8,7 +8,7 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'tabber',
+      // name: 'tabber',
       component: Tabbar,
       children: [
         {
--- topline-totiao/src/views/home/index.vue
+++ topline-totiao/src/views/home/index.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-      home
+     <!-- 导航头 -->
+     <van-nav-bar title="黑马头条"/>
+     <!-- 频道列表 -->
+     <van-tabs animated>
+        <van-tab v-for="index in 8" :title="'标签 ' + index" :key="index">
+            内容 {{ index }}
+     </van-tab>
+</van-tabs>
   </div>
 </template>
 
