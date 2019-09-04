74846ea886536dbec030bf34d25df3d71e273a1e
张晨 <502030647@example.com>
2019-09-04 17:04:14 +0800

    登陆页面的样式初步完成

--- topline-totiao/src/views/Login.vue
+++ topline-totiao/src/views/Login.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
-      login
+    <!-- 导航头 -->
+    <van-nav-bar title="登陆"/>
+    <!--输入框  -->
+    <van-cell-group>
+        <van-field v-model="value"  left-icon="phone-o" placeholder="请输入手机号码" />
+        <van-field v-model="value"  left-icon="star-o" placeholder="请输入验证码" >
+            <van-button slot="button" type="default" size='small'>发送验证码</van-button>
+        </van-field>
+    </van-cell-group>
+    <!-- 登陆按钮 -->
+    <div class="login-btn">
+        <van-button class="btn" type="info">登陆</van-button>
+    </div>
   </div>
 </template>
 
@@ -10,6 +22,11 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+    .login-btn{
+        padding: 15px;
+        .btn{
+            width: 100%;
+        }
+    }
 </style>
