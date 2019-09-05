1295b6a060da07e2c9889e506b906d010dd2fa39
张晨 <502030647@example.com>
2019-09-05 20:42:35 +0800

    封装获取文章的api接口

---
+++ topline-totiao/src/api/article.js
@@ -0,0 +1,20 @@
+import request from '../utils/request'
+
+/**
+ * 获取文章列表(登陆和不登陆)
+ * @param {*} 参数是  channel_id, timestamp, with_top
+ */
+export const getArticles = ({
+  // 频道id  时间戳  是否包含置顶1  0不包含
+  channel_id,
+  timestamp,
+  with_top
+}) => {
+  return request.get('/app/v1_1/articles'{
+      params:{
+        channel_id,
+        timestamp,
+        with_top
+      }
+  })
+}
