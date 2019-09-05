2aa3076f445d73826083695a397901a5e9215b60
张晨 <502030647@example.com>
2019-09-05 19:45:35 +0800

    首页基本布局样式完成

--- topline-totiao/src/views/home/index.vue
+++ topline-totiao/src/views/home/index.vue
@@ -59,8 +59,19 @@ export default {
 <style lang="less" scoped>
   // 在scoped中书写的样式，给动态生成的标签或者子组件中不可用
   // 深度作用选择器 /deep/
-  .van-tabs /deep/ .van-tabs__content{
-    margin-top: 46px;
+.van-tabs {
+  /deep/ .van-tabs__wrap {
+    position: fixed;
+    top: 46px;
+    left: 0;
+    z-index: 100;
+  }
+  /deep/ .van-tabs__content {
+    margin-top: 90px;
     margin-bottom: 50px;
   }
+}
+.close {
+  float: right;
+}
 </style>
