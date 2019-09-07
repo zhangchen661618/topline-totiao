96496098232273ec1302b7e846208a2dc5318dd2
张晨 <502030647@example.com>
2019-09-07 20:40:36 +0800

    点击我的频道-结束

--- topline-totiao/src/views/home/componens/ChannelEdit.vue
+++ topline-totiao/src/views/home/componens/ChannelEdit.vue
@@ -29,7 +29,7 @@
       <van-grid-item
         v-for="(channel,index) in channels"
         :key="channel.id"
-        @click="handleMyChannelItem(index)"
+        @click="handleMyChannelItem(index,channel.id)"
       >
       <div slot="text"  class="van-grid-item__text" :class="{active:active===index}">
           {{channel.name}}
@@ -57,7 +57,7 @@
 </template>
 
 <script>
-import { getAllChannels } from '../../../api/channel'
+import { getAllChannels, deleteChannel } from '../../../api/channel'
 import { mapState } from 'vuex'
 import { setItem } from '../../../utils/localStorage'
 export default {
@@ -113,7 +113,7 @@ export default {
       }
     },
     // 点击我的频道的时候
-    handleMyChannelItem (index) {
+    async  handleMyChannelItem (index, channelId) {
       // 1 非编辑模式
       if (!this.isEdit) {
         // 告诉父组件，选中频道的索引  关闭对话框
@@ -126,6 +126,11 @@ export default {
       // 判断是否登陆  通过 mapstate 做了映射
       if (this.user) {
         // 登陆 发送请求
+        try {
+          await deleteChannel(channelId)
+        } catch (err) {
+          this.$toast.fail('操作失败')
+        }
         return
       }
       // 没有登陆把频道列表纪录到本地存储中
