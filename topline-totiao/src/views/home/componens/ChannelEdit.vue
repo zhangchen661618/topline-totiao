2411ee412dfee6ad0dcecf2c696b4eb82cc48575
张晨 <502030647@example.com>
2019-09-07 15:58:23 +0800

    频道管理的界面样式

--- topline-totiao/src/views/home/componens/ChannelEdit.vue
+++ topline-totiao/src/views/home/componens/ChannelEdit.vue
@@ -3,9 +3,40 @@
     :value="value"
     @input="$emit('input',$event)"
     position="bottom"
-    :style="{ height: '80%' }"
+    :style="{ height: '90%' }"
     >
-    hhhhhh
+    <van-cell icon="cross" @click="$emit('input',false)"/>
+    <!-- 我的频道 -->
+    <van-cell title="我的频道" label="点击进入频道">
+      <van-button
+        round
+        type="danger"
+        size="mini"
+      >编辑</van-button>
+    </van-cell>
+    <van-grid>
+      <van-grid-item
+        v-for="value in 8"
+        :key="value"
+        text="文字"
+      >
+        <van-icon
+          slot="icon"
+          class="close-icon"
+          name="close"
+        />
+      </van-grid-item>
+    </van-grid>
+    <!-- 推荐频道 -->
+    <van-cell title="推荐频道" label="点击添加频道" />
+    <van-grid>
+      <van-grid-item
+        v-for="value in 8"
+        :key="value"
+        text="文字"
+      />
+    </van-grid>
+
     </van-popup>
 </template>
 
@@ -25,6 +56,10 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.close-icon {
+  position: absolute;
+  right: 0;
+  top: 0;
+}
 </style>
