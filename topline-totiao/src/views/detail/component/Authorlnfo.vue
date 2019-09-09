521a63983ee1abbee76b1cedf9247442f2f21c9c
张晨 <502030647@example.com>
2019-09-09 21:50:45 +0800

    文章详情-关注作者

--- topline-totiao/src/views/detail/component/Authorlnfo.vue
+++ topline-totiao/src/views/detail/component/Authorlnfo.vue
@@ -10,16 +10,45 @@
     <div>
       <van-button
         type="danger"
-        :loading="false"
-      >关注</van-button>
+        :loading="loading"
+        @click="handleFollow()"
+      >{{article. is_followed?'已':''}}关注</van-button>
     </div>
   </div>
 </template>
 
 <script>
+import { followUser, unFollowUser } from '../../../api/user'
 export default {
   name: 'Authorinfo',
-  props: ['article']
+  props: ['article'],
+  data () {
+    return {
+      loading: false
+    }
+  },
+  methods: {
+    // 点击按钮关注或者取消关注
+    async handleFollow () {
+      this.loading = true
+      // 判断是否登陆
+      try {
+        // 判断是否已经关注
+        if (this.article.is_followed) {
+          // 如果已经关注，则取消关注
+          await unFollowUser(this.article.aut_id)
+          this.article.is_followed = false
+        } else {
+          // 如果没有关注，则关注
+          await followUser(this.article.aut_id)
+          this.article.is_followed = true
+        }
+      } catch (err) {
+        this.$toast.fail('操作失败')
+      }
+      this.loading = false
+    }
+  }
 }
 </script>
 
