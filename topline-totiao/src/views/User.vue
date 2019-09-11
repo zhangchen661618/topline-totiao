b43ea02d9307963082aeefc8d6860daa73e7a5f7
张晨 <502030647@example.com>
2019-09-11 20:13:44 +0800

    我的页面-未登录页面

--- topline-totiao/src/views/User.vue
+++ topline-totiao/src/views/User.vue
@@ -1,15 +1,48 @@
 <template>
   <div>
-      User
+    <div class="not-login">
+        <div class="circle" @click="handleLogin">
+        <span>登录</span>
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  name: 'User',
+  methods: {
+    // 点击登录按钮，跳转到登录页面
+    handleLogin () {
+      this.$router.push({
+        path: '/login',
+        query: {
+          redirect: this.$route.fullPath
+        }
+      })
+    }
+  }
 }
 </script>
 
-<style>
+<style lang="less">
+.not-login {
+  height: 150px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .circle {
+    width: 50px;
+    height: 50px;
+    padding: 40px;
+    background-color: red;
+    border-radius: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #fff;
+    font-size: 20px;
+  }
+}
 
 </style>
