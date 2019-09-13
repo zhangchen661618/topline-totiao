2137f13f39f6c58dfd2925ff503c218fe1089298
张晨 <502030647@example.com>
2019-09-14 01:24:27 +0800

    小智机器人组件样式搭建

--- topline-totiao/src/router/index.js
+++ topline-totiao/src/router/index.js
@@ -29,6 +29,11 @@ export default new Router({
       component: () => import(/* webpackChunkName: "follw" */ '../views/follow/index.vue')
     },
     {
+      path: '/chat',
+      name: 'chat',
+      component: () => import(/* webpackChunkName: "follw" */ '../views/Chat.vue')
+    },
+    {
       path: '/user-profile',
       name: 'user-profile',
       component: () => import(/* webpackChunkName: "user-profile" */ '../views/user-profile/index.vue')
