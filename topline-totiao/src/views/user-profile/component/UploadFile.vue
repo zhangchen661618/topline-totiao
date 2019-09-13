dedef8d1c2f2f96364ff80105b96d18aaff64773
张晨 <502030647@example.com>
2019-09-13 19:14:08 +0800

    上传图片-弹出对话框

---
+++ topline-totiao/src/views/user-profile/component/UploadFile.vue
@@ -0,0 +1,27 @@
+<template>
+  <van-dialog
+  :value='value'
+  @input="$emit('input',$event)"
+  :showCancelButton='false'
+  closeOnClickOverlay
+>
+  <van-cell-group>
+    <van-cell title="从相册选者图片"  />
+    <van-cell title="拍照" />
+    <van-cell title="取消" @click="$emit('input',false)"/>
+  </van-cell-group>
+</van-dialog>
+</template>
+
+<script>
+export default {
+  name: 'UploadFile',
+  props: ['value']
+}
+</script>
+
+<style lang='less' scoped>
+  .van-cell__title{
+    text-align: center;
+  }
+</style>
