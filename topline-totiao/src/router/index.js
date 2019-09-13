13b94609dc7ffd84194bea90add4b8138aafa99d
张晨 <502030647@example.com>
2019-09-13 21:52:56 +0800

    点击关注和粉丝的组件搭建

--- topline-totiao/src/router/index.js
+++ topline-totiao/src/router/index.js
@@ -24,6 +24,11 @@ export default new Router({
       ]
     },
     {
+      path: '/follow',
+      name: 'follow',
+      component: () => import(/* webpackChunkName: "follw" */ '../views/Follow.vue')
+    },
+    {
       path: '/user-profile',
       name: 'user-profile',
       component: () => import(/* webpackChunkName: "user-profile" */ '../views/user-profile/index.vue')
---
+++ topline-totiao/src/views/Follow.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+      <!-- 导航头 -->
+    <van-nav-bar
+        title="关注/粉丝"
+        left-arrow
+        @click-left="$router.back()"
+    />
+      <!-- tabs -->
+    <van-tabs v-model="active">
+        <van-tab title="关注">内容 1</van-tab>
+        <van-tab title="粉丝">内容 2</van-tab>
+    </van-tabs>
+      <!-- list 列表 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Follow',
+  data () {
+    return {
+      active: 1
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
